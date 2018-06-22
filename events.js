c692ef1847956e5285aee041e7b47973b03fc4ca
David Meister <thedavidmeister@gmail.com>
2018-06-22 20:33:55 +1000

    tweak form submit tracking to delegate to document

--- events.js
+++ events.js
@@ -1,30 +1,34 @@
+var matches = function(el, selector) {
+  return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
+};
+
 document.addEventListener('DOMContentLoaded', function() {
-  // generic click event tracking
+  // delegate click tracking to the document body
   document.body.addEventListener('click', function(e) {
-    var target = e.target;
+    // generic click event tracking
+    var el = e.target;
     var category = 'dom-element';
     var action = 'click';
     var label = '';
-    if (target.tagName) {
+    if (el.tagName) {
      label += target.tagName;
     }
-    if (target.id) {
+    if (el.id) {
      label += '#' + target.id ;
     }
-    if (target.className) {
+    if (el.className) {
      label += '.' + target.className.replace(' ', '.');
     }
     console.debug('GA tracking click event:', category, action, label);
     ga('send', 'event', category, action, label);
-    return true;
-  });
 
-  // track all form submits
-  // squarespace hijacks form submit, so we work off clicking submit buttons
-  document.querySelectorAll('[type="submit"]').forEach(function(el) {
-    el.addEventListener('click', function(e) {
+    // track all form submits
+    // squarespace hijacks form submit, so we work off clicking submit buttons
+    if (matches(el, '[type="submit"]')) {
       console.debug('GA tracking form submit button clicked');
       ga('send', 'event', 'form', 'submit');
-    });
+    }
+
+    return true;
   });
 });
